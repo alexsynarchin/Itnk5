2fded0d625df1f4c675f8fc8f4b0c64045eed29b
alexsynarchin <alexsynarchin@gmail.com>
2016-01-12 11:10:12 +0500

    Иконка для пользователя

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="form-group has-feedback">
                 <input type="username" class="form-control" placeholder="Введите логин">
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
                 <input type="password" class="form-control" placeholder="Введите пароль">
