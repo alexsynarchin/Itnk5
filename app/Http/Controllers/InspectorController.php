2d29ac2027a54d229426127b9ff4397bae7ada1b
alexsynarchin <alexsynarchin@gmail.com>
2016-04-25 11:06:49 +0500

    коммит по выгрузке в excel

--- app/Http/Controllers/InspectorController.php
+++ app/Http/Controllers/InspectorController.php
@@ -52,17 +52,31 @@ class InspectorController extends Controller
         $file = Excel::create($filename, function($excel)use($report) {
             $excel->sheet('Сводные данные по отчету', function($sheet)use($report) {
                 $sheet->mergeCells('A1:E1');
+                $sheet->mergeCells('A5:E5');
+                $sheet->mergeCells('A10:E10');
+                $sheet->mergeCells('A14:E14');
                 $sheet->fromArray(array(
                     array('Итоговые суммы по отчету'),
                     array('Балансовая стоимость', 'Начисленный износ', 'Сумма списания', 'Остаточная стоимость'),
-                    array($report->report_total_carrying_amount, $report->report_wearout_value, $report->decommission_carrying_amount, $report->report_total_residual_value)
-                ), null, 'A2', false, false);
-                array('');
-                array('Сводные данные по прибретению');
+                    array($report->report_total_carrying_amount, $report->report_wearout_value, $report->decommission_carrying_amount, $report->report_total_residual_value),
+                    array(''),
+                    array('Сводные данные по приобретению'),
+                    array('','Итого','	Движимое имущество','Особо ценное движимое имущество','	Здания и сооружения','Земельные участки','Автомобили'),
+                    array('Балансовая стоимость',$report->report_carrying_amount,$report->report_movables_carrying_amount,$report->report_value_movables_carrying_amount,$report->report_buildings_carrying_amount,$report->report_parcels_carrying_amount,$report->report_cars_carrying_amount),
+                    array('Остаточная стоимость',$report->report_residual_value,$report->report_movables_residual_value,$report->report_value_movables_residual_value,$report->report_buildings_residual_value,$report->report_parcels_residual_value,$report->report_cars_residual_value),
+                    array(''),
+                    array('Сводные данные по начислению износа'),
+                    array('Балансовая стоимость','Начисление износа','Остаточная стоимость'),
+                    array($report->report_wearout_carrying_amount,$report->report_wearout_value,$report->report_wearout_residual_value),
+                    array(''),
+                    array('Сводные данные по списанию основных средств'),
+                    array('Балансовая стоимость','Сумма списания'),
+                    array($report->decommission_carrying_amount,$report->decommission_sum)
+                ), null, 'A1', false, false);
             });
 
 
-            $excel->sheet('Итоговые данные по отчету', function($sheet)use($report) {
+            $excel->sheet('Раздел приобретение', function($sheet)use($report) {
 
                 $sheet->fromArray(array(
                     array('Балансовая стоимость', 'Начисленный износ', 'Сумма списания', 'Остаточная стоимость'),
