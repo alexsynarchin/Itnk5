c878a3daf63b67ac62118a137e2fd1adcd94b8d7
alexsynarchin <alexsynarchin@gmail.com>
2016-05-03 19:32:42 +0500

    телефон в подвале сайта

--- resources/views/layouts/partials_dashboard/footer.blade.php
+++ resources/views/layouts/partials_dashboard/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        Телефоны: +7 (347) 266-15-33, +7913426471
+        Телефоны: +7 (347) 266-15-33, +79173426471
     </div>
     <!-- Default to the left -->
     <strong>Разработка компании ИТНК &copy; 2015 </strong>
