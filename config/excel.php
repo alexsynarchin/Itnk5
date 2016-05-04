dff915ee4e6740a2fb23ff547c3721261de0987d
alexsynarchin <alexsynarchin@gmail.com>
2016-05-04 11:01:38 +0500

    коммит

--- config/excel.php
+++ config/excel.php
@@ -31,7 +31,7 @@ return array(
         */
         'settings' => array(
 
-            'memoryCacheSize' => '32MB',
+            'memoryCacheSize' => '64MB',
             'cacheTime'       => 600
 
         ),
